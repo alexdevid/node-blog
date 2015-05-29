f1c15b3c86245ce00227ef00e7f2046e61bd8567
Alexander <kerdevid@gmail.com>
2015-05-29 17:50:20 +0300

    routes

--- routes/web.js
+++ routes/web.js
@@ -1,12 +1,34 @@
 module.exports = function(app, models) {
 	app.get('/', function(req, res) {
+		res.render('web/index', {
+			title: "Home",
+			layout: 'web/layout'
+		});
+	});
+	app.get('/about', function(req, res) {
+		res.render('web/about', {
+			title: "About",
+			layout: 'web/layout'
+		});
+	});
+
+	app.get('/blog', function(req, res) {
 		models.article.find({}, function(err, posts) {
-			res.render('web/index', {
+			res.render('web/blog', {
 				title: "My Blog",
 				layout: 'web/layout',
 				posts: posts
 			});
 		});
+	});
 
+	app.get('/blog/:id', function(req, res) {
+		models.article.find({id: req.params.id}, function(err, post) {
+			res.render('web/blog-post', {
+				layout: 'web/layout',
+				title: post[0].title,
+				post: post[0]
+			});
+		});
 	});
 };
